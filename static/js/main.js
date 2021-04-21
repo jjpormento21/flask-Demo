b0b563cffbf97ec1ab03708f9c3e1371fe1aa9ea
jjpormento21 <jonahjamespormento21@gmail.com>
2021-04-21 16:23:06 +0800

    fixed delete bug

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,8 @@ $(navLinks[pageData.pageid]).addClass("active");
 let today = new Date();
 var element = document.getElementById("dateEdited");
 element.value = today.toString();
+
+function setPostToDelete(postId) {
+    document.querySelector(".delete-post-btn").setAttribute("href", '/blog_post_delete/' + postId);
+    console.log(postId);
+}
