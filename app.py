7fcb98474aa79dca9720c180f9dca04de88f6f5a
jjpormento21 <jonahjamespormento21@gmail.com>
2021-06-16 19:06:18 +0800

    security test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from datetime import datetime
+from flask_talisman import Talisman
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,6 +10,7 @@ import os
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = str(os.getenv('MONGO_URI'))
+Talisman(app)
 mongo = PyMongo(app)
 # Collections (tables)
 taskMaster = mongo.db.taskmaster
